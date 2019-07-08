e7a3823a00873182d04ba08856c0f090e1462b7b
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-08 17:03:44 -0600

    Created the Home component and set as the home route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import logo from './logo.svg';
-import './App.scss';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import './App.scss'
+import Home from './components/Home'
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <h1> Welcome Home </h1>
+      </header>
+    </div>
+  )
+}
+
+export default Home
