69d0e7e5148a180476291c0ceac7757252e46460
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-08 17:26:03 -0600

    Created Navigation component and route

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import './App.scss'
 import Home from './components/Home'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
     <Router>
+        <Route exact path="/" component={Navigation} />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <h1> Welcome Home </h1>
+      </header>
+    </div>
+  )
+}
+
+export default Navigation
