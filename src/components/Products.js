7aa27f22397df9443c0055d37f63db6395199959
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-08 20:15:35 -0600

    Created a fetch to get products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const Products = () => {
-  return (
-    <div>
-      <header className="App-header">
-        <h1> Products </h1>
-      </header>
-    </div>
-  )
+class Products extends Component {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    console.log('mounted')
+
+    fetch('http://localhost:4000/api/products')
+    .then(res => res.json())
+    .then(products => {
+      console.log(products)
+      this.setState({
+        products: products
+      }, () => {
+          // console.log("products111", this.state.products[0])
+      })
+    })
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1> Products </h1>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default Products
