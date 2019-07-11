cfece18942214a314d14c9945c562c8290334db9
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-11 13:07:39 -0600

    Wrapped the productDetail in a div and reference name correctly

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -1,5 +1,4 @@
-import React, {Component} from 'react'
-
+import React, { Component } from 'react'
 import Card from '../Card'
 
 class ProductDetail extends Component {
@@ -13,26 +12,29 @@ class ProductDetail extends Component {
     console.log("id", id)
 
     fetch(`http://localhost:4000/api/products/${id}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log("data", data)
-      this.setState({
-        product: data.product
+      .then(res => res.json())
+      .then(data => {
+        console.log("data", data)
+        this.setState({
+          product: data.product
+        })
       })
-    })
-   .catch(error => {
-     console.log(error)
-   })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
   render() {
     return (
-      <Card>
-      <h2> Product ID: {this.state.product.name} </h2>
-        <img src={this.state.product.img_url} alt="product" />
-        {/* <p> {name} </p> */}
-        <p> Price: ${this.state.product.price / 100}.00 </p>
-      </Card>
+      <div>
+        <h2> Product Detail </h2>
+        <Card>
+          <h2> Product ID: {this.state.product.name} </h2>
+          <img src={this.state.product.img_url} alt="product" />
+          <p> {this.state.product.description} </p>
+          <h3> Price: ${this.state.product.price / 100}.00 </h3>
+        </Card>
+      </div>
     )
   }
 }
