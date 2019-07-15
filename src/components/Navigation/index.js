d92938bcad94032374cb574a8b57c47db604c147
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-14 23:12:35 -0600

    Added a new class so the 2nd link's padding-right would be longer

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,12 +5,12 @@ import styles from './navigation.module.scss'
 const Navigation = () => {
   return (
     <nav className={styles.navi}>
-      <div className={styles.item}>
-        <Link to='/'> Home </Link>
-      </div>
-      <div className={styles.item}>
-        <Link to='/products'> Products </Link>
-      </div>
+      {/* <div className={styles.link1}> */}
+        <Link className={styles.link} to='/'> Home </Link>
+      {/* </div> */}
+      {/* <div className={styles.link1}> */}
+        <Link className={styles.link2} to='/products'> Products </Link>
+      {/* </div> */}
     </nav>
   )
 }
