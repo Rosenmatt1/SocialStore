7533d6ac554e0b4bbfa194a6f0703a85984f3779
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-18 16:29:45 -0600

    Added protected route for Profile and and test

--- src/components/ExternalApi/index.js
+++ src/components/ExternalApi/index.js
@@ -4,21 +4,34 @@ import React, { useState } from "react";
 import { useAuth0 } from "../../react-auth0-wrapper";
 
 const ExternalApi = () => {
-  const [showResult, setShowResult] = useState(false);
-  const [apiMessage, setApiMessage] = useState("");
-  const { getTokenSilently } = useAuth0();
+  const [showResult, setShowResult] = useState(false)
+  const [apiMessage, setApiMessage] = useState("")
+  const { getTokenSilently } = useAuth0()
 
   const callApi = async () => {
-    try {
-      const token = await getTokenSilently();
+// PROMISE CHAIN SYNTAX , instead of async await
+    // fetch('http://localhost:4000/api/external', {
+    //   headers: {
+    //     Authorization: `Bearer ${token}`
+    //   }
+    // })
+    //   .then(response => response.json())
+    //   .then(responseData => {
+    //     setShowResult(true)
+    //     setApiMessage(responseData)
+    //   })
+    //   .catch(error => console.error(error))
 
-      const response = await fetch("/api/external", {
+    try {
+      const token = await getTokenSilently()
+
+      const response = await fetch("http://localhost:4000/api/external", {
         headers: {
           Authorization: `Bearer ${token}`
         }
-      });
+      })
 
-      const responseData = await response.json();
+      const responseData = await response.json()
 
       setShowResult(true);
       setApiMessage(responseData);
@@ -28,11 +41,11 @@ const ExternalApi = () => {
   };
 
   return (
-    <>
+    <div>
       <h1>External API</h1>
       <button onClick={callApi}>Ping API</button>
       {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
-    </>
+    </div>
   );
 };
 
