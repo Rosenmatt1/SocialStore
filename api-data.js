4b673d30e198ff7885e6e802f74139f0a0f7dd7c
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-08-01 14:38:27 -0600

    Created an api data file to make new objects for the api to fetch so the store has photos that represent what an atual store would look like

---
+++ api-data.js
@@ -0,0 +1,10 @@
+const apiData = [
+  {
+    "id": 1,
+    "name": "Awesome Shirt",
+    "description": "Sapiente ut sed labore. Omnis fuga exercitationem explicabo omnis laboriosam. Minima eum consequuntur et illum.",
+    "price": 1500,
+    "img_url": "http://lorempixel.com/640/480",
+    "category_id": 1
+  }
+]
