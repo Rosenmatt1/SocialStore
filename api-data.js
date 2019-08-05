bb2eb5010e4b34f9487bfd0fa65208586e834f97
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-08-05 11:31:35 -0600

    Created 6 dumby pieces of products of hats for the api-data

--- api-data.js
+++ api-data.js
@@ -4,7 +4,49 @@ const apiData = [
     "name": "Red Hat",
     "description": "One of our most stylish and best selling.",
     "price": 5,
-    "img_url": "https://www.augustcaps.com/assets/images/2019/0703%20jr%20paisley/thumbnails/kids%20jr%20wholesale%20snapback%20paisley%20red%20_thumbnail.jpghttps://www.augustcaps.com/assets/images/2019/0703%20jr%20paisley/thumbnails/kids%20jr%20wholesale%20snapback%20paisley%20red%20_thumbnail.jpg",
+    "img_url": "https://www.augustcaps.com/assets/images/2019/0703%20jr%20paisley/thumbnails/kids%20jr%20wholesale%20snapback%20paisley%20red%20_thumbnail.jpg",
+    "category_id": 1
+  },
+  {
+    "id": 2,
+    "name": "Pineapple Hat",
+    "description": "The hat if you love pineapples.",
+    "price": 5,
+    "img_url": "https://www.augustcaps.com/assets/images/2019/0703%20jr%20coconut/thumbnails/KIDS%20JR%20Wholesale%20Coconut%20snapback%20Pink%20_thumbnail.jpg",
+    "category_id": 1
+  },
+  {
+    "id": 3,
+    "name": "Beach Hat",
+    "description": "Beach mesh hat.",
+    "price": 5,
+    "img_url": "https://www.augustcaps.com/assets/images/2019/0711%20trucker%20coconut%20tree/thumbnails/Wholesale%20Mesh%20Trucker%20snapback%20Coconut%20tree%20-%20Yellow%20Green_thumbnail.jpg",
+    "category_id": 1
+  },
+  {
+    "id": 4,
+    "name": "Cammo Hat",
+    "description": "The stylish cammo hat to bring you on any adventure.",
+    "price": 5,
+    "img_url": "https://www.augustcaps.com/assets/images/2017/trucker%205pan%20solid/thumbnails/wholesale%20blank%20mesh%205%20panel%20trucker%20solid%20hat%20camo%2002_thumbnail.jpg",
+    "category_id": 1
+  },
+  {
+    "id": 5,
+    "name": "Universe Hat",
+    "description": "Wear the universe on your head.",
+    "price": 5,
+    "img_url": "https://www.augustcaps.com/assets/images/2017/trucker%20design/thumbnails/wholesale%205%20panel%20plain%20trucker%20galaxy%20rainbow%20hat%2002_thumbnail.jpg",
+    "category_id": 1
+  },
+  {
+    "id": 6,
+    "name": "Trucker Hat",
+    "description": "The classic trucker hat.",
+    "price": 5,
+    "img_url": "https://www.augustcaps.com/assets/images/2017/trucker%205pan%20solid/thumbnails/wholesale%20blank%20mesh%205%20panel%20trucker%20solid%20hat%20black%2002_thumbnail.jpg",
     "category_id": 1
   }
 ]
+
+
