0e2c245d92a560c991b9d16c0e101c399f6f661c
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-08-08 15:12:09 -0600

    Added more tests in Navigation

--- src/components/__tests__/Navigation.test.js
+++ src/components/__tests__/Navigation.test.js
@@ -2,15 +2,48 @@ import React from 'react'
 // import App from '../../App'
 import Navigation from '../Navigation'
 import { shallow } from 'enzyme'
-import * as Auth0 from "../../react-auth0-wrapper";
+import * as Auth0 from "../../react-auth0-wrapper"
+import { Link } from 'react-router-dom'
 
-test('<Navigation /> renders without crashing', () => {
+test('<Navigation /> renders 3 Link components', () => {
   jest.spyOn(Auth0, 'useAuth0').mockImplementation(() => {
     return {
       isAuthenticated: false
     }
   })
-
   const wrapper = shallow(<Navigation />)
-
+  expect(wrapper.find(Link).length).toBe(3)
 })
+
+it('<Navigation /> renders 1 button when isAuthenticaed = false', () => {
+  jest.spyOn(Auth0, 'useAuth0').mockImplementation(() => {
+    return {
+      isAuthenticated: false
+    }
+  })
+  const wrapper = shallow(<Navigation />)
+  expect(wrapper.find('button').length).toBe(1)
+})
+
+it('<Navigation /> renders 1 button when isAuthenticaed = false', () => {
+  jest.spyOn(Auth0, 'useAuth0').mockImplementation(() => {
+    return {
+      isAuthenticated: true
+    }
+  })
+  const wrapper = shallow(<Navigation />)
+  expect(wrapper.find('button').length).toBe(1)
+  expect(wrapper.find('button').html()).toMatch('Log Out')
+})
+
+it('<Navigation /> renders 4 Link components', () => {
+  jest.spyOn(Auth0, 'useAuth0').mockImplementation(() => {
+    return {
+      isAuthenticated: true
+    }
+  })
+  const wrapper = shallow(<Navigation />)
+  expect(wrapper.find(Link).length).toBe(4)
+})
+
+
