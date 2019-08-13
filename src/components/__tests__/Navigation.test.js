7c99cd3c1493d91b9c786f7cbbe0cfb0f81431b3
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-08-13 12:49:29 -0600

    Made the test decription more terse becasue the other commentary was in the describe

--- src/components/__tests__/Navigation.test.js
+++ src/components/__tests__/Navigation.test.js
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom'
 describe('<Navigation /> functionality', () => {
   describe('isAuthenticated = false', () => {
 
-    test('renders 3 Link components', () => {
+    it('renders 3 Link components', () => {
       jest.spyOn(Auth0, 'useAuth0').mockImplementation(() => {
         return {
           isAuthenticated: false
@@ -29,7 +29,8 @@ describe('<Navigation /> functionality', () => {
   })
 
   describe('isAuthenticated = true', () => {
-    it('<Navigation /> renders 1 button when isAuthenticaed = false', () => {
+
+    it('<Navigation /> renders 1 button', () => {
       jest.spyOn(Auth0, 'useAuth0').mockImplementation(() => {
         return {
           isAuthenticated: true
@@ -39,7 +40,6 @@ describe('<Navigation /> functionality', () => {
       expect(wrapper.find('button').length).toBe(1)
       // expect(wrapper.find('button').html()).toMatch('Log Out')
     })
-
     it('renders 4 Link components', () => {
       jest.spyOn(Auth0, 'useAuth0').mockImplementation(() => {
         return {
